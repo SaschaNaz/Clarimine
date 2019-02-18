a20134961a7977e64cf22b7624343de0939a4a3f
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-02-18 16:03:15 +0900

    Update MBC.ts

--- src/impl/MBC.ts
+++ src/impl/MBC.ts
@@ -4,14 +4,14 @@ import { Article } from 'index';
 
 export function parse(): Article {
     return {
-        title: $('#content .view-title').text(),
-        content: clearStyles($('#DivPrint .view-con')[0].cloneNode(true)).innerHTML,
+        title: $('#news_content_1 .title').text().trim(),
+        content: clearStyles($("#news_content_1 .body")[0].cloneNode(true)).innerHTML,
         timestamp: {
-            created: new Date($('#DivPrint .article-time-date').text().replace(/-/g, '/')),
-            lastModified: undefined
+            created: new Date($("#news_content_1 .wrap_time span")[1].childNodes[1].textContent.trim().replace(/-/g, '/')),
+            lastModified: new Date($("#news_content_1 .wrap_time span")[2].childNodes[1].textContent.trim().replace(/-/g, '/'))
         },
         reporters: [{
-            name: $('#DivPrint .reporter').text().trim().split(/\s+/)[0],
+            name: $("#news_content_1 .wrap_time span:first-child").text().trim().split(/\s+/)[0],
             mail: undefined
         }]
     };
